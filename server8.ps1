26a7f1cdf0f961e04da77f46b1ceb3bf3738e864
Einar Arnar Magnússon <einarma246@nemi.tskoli.is>
2017-04-06 11:25:41 +0000

    server8 update

--- server8.ps1
+++ server8.ps1
@@ -1,3 +1,9 @@
+#NETWORK
+Rename-NetAdapter -Name "Ethernet 2" -NewName "LAN"
+New-NetIPAddress -InterfaceAlias "LAN" -IPAddress 172.16.24.2 -PrefixLength 21
+Set-DnsClientServerAddress -InterfaceAlias "LAN" -ServerAddresses 127.0.0.1
+
+
 ## Installa AD + DC
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName "tskoli.win3b" -InstallDns -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "pass.123" -Force)
