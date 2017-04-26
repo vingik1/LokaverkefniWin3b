811b74d8f31c1c17d4df5df60fa404a527c71b0b
Viktor Ingi Kárason <vingik@gmail.com>
2017-04-26 16:12:14 +0000

    byrja á OU setup

---
+++ NotendaSetup.ps1
@@ -0,0 +1,29 @@
+﻿cd C:\Users\Administrator\Documents
+$notendur = Import-Csv -Path "lokaverk_notendur_u.csv"
+
+New-ADOrganizationalUnit -name WIN3B_Tskoli -ProtectedFromAccidentalDeletion $false
+New-ADGroup -name AllirTskoli -Path "OU=WIN3B_Tskoli,DC=tskoli,DC=local" -GroupScope Global
+foreach($n in $notendur){
+    $skoli = $n.'Skóli;Deild;Nafn'.Split(";")[0]
+    $deild = $n.'Skóli;Deild;Nafn'.Split(";")[1]
+    $nafn = $n.'Skóli;Deild;Nafn'.Split(";")[2]
+
+    if((Get-ADOrganizationalUnit -Filter {name -eq $skoli }).Name -ne $skoli) {
+
+        New-ADOrganizationalUnit -name $skoli -Path "OU=WIN3B_Tskoli,DC=tskoli,DC=local" -ProtectedFromAccidentalDeletion $false
+        New-ADGroup -Name $skoli -Path $("OU="+$skoli+",OU=WIN3B_Tskoli,DC=tskoli,DC=local") -GroupScope Global
+        Add-ADGroupMember -Identity AllirTskoli -Members $skoli
+        }
+        
+}
+
+foreach($n in $notendur){
+            $skoli = $n.'Skóli;Deild;Nafn'.Split(";")[0]
+    $deild = $n.'Skóli;Deild;Nafn'.Split(";")[1]
+    $nafn = $n.'Skóli;Deild;Nafn'.Split(";")[2]
+    if((Get-ADOrganizationalUnit -SearchBase $("OU="+$skoli+",OU=WIN3B_Tskoli,DC=tskoli,DC=local") -Filter {name -eq $deild }).Name -ne $deild) {
+
+        New-ADOrganizationalUnit -name $deild -Path $("OU="+$skoli+",OU=WIN3B_Tskoli,DC=tskoli,DC=local") -ProtectedFromAccidentalDeletion $false
+        New-ADGroup -Name $deild -Path $("OU="+$deild+",OU="+$skoli+",OU=WIN3B_Tskoli,DC=tskoli,DC=local") -GroupScope Global
+        }
+}
