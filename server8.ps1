56e43f9af3cf065c6ad6163acddf1e37e8515fa6
Viktor Ingi Kárason <vingik@gmail.com>
2017-04-27 11:46:58 +0000

    bæta við ou skolar og deildir í server 8

--- server8.ps1
+++ server8.ps1
@@ -7,3 +7,34 @@ Set-DnsClientServerAddress -InterfaceAlias "LAN" -ServerAddresses 127.0.0.1
 ## Installa AD + DC
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest -DomainName "tskoli.win3b" -InstallDns -SafeModeAdministratorPassword (ConvertTo-SecureString -AsPlainText "pass.123" -Force)
+
+#bæta við ou skolar og deildir
+cd C:\Users\Administrator\Documents
+$notendur = Import-Csv -Path "lokaverk_notendur_u.csv"
+
+New-ADOrganizationalUnit -name WIN3B_Tskoli -ProtectedFromAccidentalDeletion $false
+New-ADGroup -name AllirTskoli -Path "OU=WIN3B_Tskoli,DC=tskoli,DC=win3b" -GroupScope Global
+foreach($n in $notendur){
+    $skoli = $n.'Skóli;Deild;Nafn'.Split(";")[0]
+    $deild = $n.'Skóli;Deild;Nafn'.Split(";")[1]
+    $nafn = $n.'Skóli;Deild;Nafn'.Split(";")[2]
+
+    if((Get-ADOrganizationalUnit -Filter {name -eq $skoli }).Name -ne $skoli) {
+
+        New-ADOrganizationalUnit -name $skoli -Path "OU=WIN3B_Tskoli,DC=tskoli,DC=win3b" -ProtectedFromAccidentalDeletion $false
+        New-ADGroup -Name $skoli -Path $("OU="+$skoli+",OU=WIN3B_Tskoli,DC=tskoli,DC=win3b") -GroupScope Global
+        Add-ADGroupMember -Identity AllirTskoli -Members $skoli
+        }
+        
+}
+
+foreach($n in $notendur){
+            $skoli = $n.'Skóli;Deild;Nafn'.Split(";")[0]
+    $deild = $n.'Skóli;Deild;Nafn'.Split(";")[1]
+    $nafn = $n.'Skóli;Deild;Nafn'.Split(";")[2]
+    if((Get-ADOrganizationalUnit -SearchBase $("OU="+$skoli+",OU=WIN3B_Tskoli,DC=tskoli,DC=win3b") -Filter {name -eq $deild }).Name -ne $deild) {
+
+        New-ADOrganizationalUnit -name $deild -Path $("OU="+$skoli+",OU=WIN3B_Tskoli,DC=tskoli,DC=win3b") -ProtectedFromAccidentalDeletion $false
+        New-ADGroup -Name $deild -Path $("OU="+$deild+",OU="+$skoli+",OU=WIN3B_Tskoli,DC=tskoli,DC=win3b") -GroupScope Global
+        }
+}
